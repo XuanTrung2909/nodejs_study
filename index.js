9ec06a662fc81b8424dc7645f19092fb77bdddb4
trung.nx2909 <trung.nx290989@gmail.com>
2021-09-20 11:26:31 +0700

    end buoi 6

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const express = require("express");
+const path = require("path");
 const { rootRouter } = require("./src/Routers/RootRouter");
 
 const app = express();
+
+//setup statuc file
+
+const publicPathDirectory = path.join(__dirname, "./public");
+
+app.use("/public", express.static(publicPathDirectory));
+
+//truy cap hinh
+
+/**
+ *
+ * http://localhost:9000 <=> public
+ *
+ */
+
+//setup json
 
 app.use(express.json());
 
